cd77754d37e1a1b0a17e2b2b337eb168d0d29036
James Mahoney <mahoneyjames@gmail.com>
2017-06-05 09:11:18 +0100

    node blog build

--- build/build.js
+++ build/build.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const pug = require('pug');
+const showdown = require('showdown');
+const jfile = require('jfile');
+const frontmatter = require('front-matter');
            
 const helpers = require('./helpers');
            
+const posts = [
+    generateFileHtmlFromMarkdown("post",'_posts/2015-07-21-welcome.md','posts/2015-07-21-welcome',{} ),
+    generateFileHtmlFromMarkdown("post",'_posts/2015-07-23-acra_email_alerts.md','posts/2015-07-23-acra_email_alerts',{} ),
+    generateFileHtmlFromMarkdown("post",'_posts/2016-09-02-google_sheets_backend_data_store_via_json.md','posts/2016-09-02-google_sheets_backend_data_store_via_json',{} )
+    ];
 
-generateHtml("index", "index", {});
+generateHtml("index", "index", {posts});
 generateHtml("about","about",{title:"About"});
     
 function generateHtml(view, outputFile, options)
@@ -13,10 +21,43 @@ function generateHtml(view, outputFile, options)
     options.helpers = helpers; 
     options.siteRoot = "";   
     const html = pug.renderFile(`${__dirname}/views/${view}.pug`,options);
-    fs.writeFileSync(`./_generated/${outputFile}.htm`, html, 'utf-8');
+
+    const fullOutputFileName =`./_generated/${outputFile}.htm`;    
+    helpers.ensureDirectoryExists(fullOutputFileName);
+    fs.writeFileSync(fullOutputFileName, html, 'utf-8');
 }
 
 function sanitisePath(path)
 {
     return path.split(" ").join("-") + ".htm";
 }
+
+
+
+function generateFileHtmlFromMarkdown(view, sourceFile, outputFile, options)
+{
+    const converter = new showdown.Converter();
+    const content = frontmatter(helpers.readFile(sourceFile));
+    options.body = converter.makeHtml(content.body);
+    console.log(content.attributes);
+    options.attributes = content.attributes;
+    options.title = options.attributes.title;
+    generateHtml(view, outputFile,options);
+
+    const postDetails = {
+        path: `/${outputFile}.htm`,
+        title: content.attributes.title
+    };
+
+    return postDetails;
+
+}
+
+function htmlFromMarkdown(sourceFile){
+
+    const filecontents = helpers.readFile(sourceFile);
+
+    return converter.makeHtml(filecontents);
+}
+//console.log(htmlFromMarkdown('_posts/2015-07-21-welcome.md'));
+
--- build/helpers.js
+++ build/helpers.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const moment = require('moment');
+const mkdirp = require('mkdirp');
+
 exports.moment = require('moment');
 
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
 
-exports.siteName='Story Marmalade'; 
+exports.siteName='James Mahoney'; 
 
 exports.storyBody  = (story, rule) =>
 {
@@ -33,3 +35,23 @@ exports.friendlyDateLong = (date)=>
 {
     return moment(date).format("MMM Do YYYY");
 };
+
+exports.readFile = (sourcefile) =>
+{
+     const filename = `${__dirname}/${sourcefile}`;
+    try
+    {
+        return fs.readFileSync(filename,'utf-8')
+    }
+    catch(err)
+    {
+        //TODO - really should learn about better error handling in node...
+        console.log(`Failed to find file '${filename}'`);
+        return `Failed to find file '${filename}'`;
+    }
+}
+
+exports.ensureDirectoryExists = (directory)=>{
+    const path = directory.split('/').slice(0,-1).join('/');
+    mkdirp(path); 
+}
